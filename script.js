e9dfa2c626d80d6023971d8d0817363148f5dfa2
ol3gatron <olegatron@gmail.com>
2022-01-21 18:42:24 +0300

    started a new brunch

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-// let popup = prompt("Enter a grid size. Example: 4 -> 4x4")
+gridItem = document.createElement("div");
+container = document.querySelector(".container");
 
-const grid = document.querySelector(".grid");
-const resetButton = document.createElement("button");
-const btn = document.querySelector(".reset");
-
-
-
-function createGridCell(x) {
-  for (i = 0; i < x; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-cell");
-    grid.appendChild(gridItem);
-  }
-}
-
-createGridCell(16*16);
-
-const gridCells = document.querySelectorAll(".grid-cell");
-gridCells.forEach((gridCells) => {
-  gridCells.addEventListener("mouseover", () => {
-    gridCells.classList.add("grid-cell-black");
-  });
-});
-
-
-btn.addEventListener("click", () => {
-  gridCells.forEach((gridCells) => {
-    gridCells.classList.remove("grid-cell-black");
-  });
-});
+container.appendChild("div");
