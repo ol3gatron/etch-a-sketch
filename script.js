eeef237efd173e1a727537da49754c5ff2730f61
ol3gatron <olegatron@gmail.com>
2022-01-22 13:24:05 +0300

    added a function that returns a random color

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-let fieldSize = prompt("Введите размер сетки")
+let fieldSize = prompt("Enter field size")
 
 function createRows(y) {
   for (let i = 0; i < y; i++) {
@@ -18,11 +18,38 @@ function createColumns(x) {
   }
 }
 
+function randomColor() {
+  let random = Math.floor(Math.random() * 7 + 1);
+  switch (random) {
+    case 1:
+      return "red";
+      break;
+    case 2:
+      return "orange";
+      break;
+    case 3:
+      return "yellow";
+      break;
+    case 4:
+      return "green";
+      break;
+    case 5:
+      return "deepskyblue";
+      break;
+    case 6:
+      return "blue";
+      break;
+    case 7:
+      return "#8b00ff";
+      break;
+  }
+}
+
 createColumns(fieldSize)
 
 const rows = document.querySelectorAll(".row");
 rows.forEach(row => {
   row.addEventListener("mouseover", () => {
-    row.classList.toggle("black")
+    row.style.cssText = `background-color: ${randomColor()}`
   })
 });
