5ce2dcba5f16d0b18c2fdded942da243518c60ae
ol3gatron <olegatron@gmail.com>
2022-01-22 12:31:11 +0300

    added color change of grid item in hover

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-container = document.querySelector(".container");
+const container = document.querySelector(".container");
+let fieldSize = prompt("Введите размер сетки")
 
-function createRows(x) {
-  for (let i = 0; i < x; i++) {
-    gridItem = document.createElement("div");
-    gridItem.classList.add("fieldItem")
-    container.appendChild(gridItem)
+function createRows(y) {
+  for (let i = 0; i < y; i++) {
+    row = document.createElement("div")
+    row.classList.add("row")
+    column.appendChild(row)
   }
 }
 
-createRows(4);
+function createColumns(x) {
+  for (let i = 0; i < x; i++) {
+    column = document.createElement("div");
+    column.classList.add("column")
+    container.appendChild(column)
+    createRows(x)
+  }
+}
+
+createColumns(fieldSize)
+
+const rows = document.querySelectorAll(".row");
+rows.forEach(row => {
+  row.addEventListener("mouseover", () => {
+    row.classList.toggle("black")
+  })
+});
